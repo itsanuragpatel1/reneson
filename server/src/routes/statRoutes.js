2dc71e1ec6034c77226c4c8ff7f2b3571bc8f835
Anurag <anuragpateloriginal@gmail.com>
2026-03-10 16:15:10 +0530

    fix stats controller req.body parsing

--- server/src/routes/statRoutes.js
+++ server/src/routes/statRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { createStat, deleteStat, getStats, updateStat } from "../controllers/statController.js";
 import { adminAuth } from "../middleware/adminAuth.js";
+import { upload } from "../middleware/multerMiddleware.js";
 
 const statRoutes = express.Router();
 
-statRoutes.post("/create",adminAuth,createStat);
+// upload.none() parses multipart/form-data requests without files so req.body works correctly
+
+statRoutes.post("/create",adminAuth,upload.none(),createStat);  
 statRoutes.get("/all", getStats);
-statRoutes.put("/:id", adminAuth, updateStat);
+statRoutes.put("/:id", adminAuth, upload.none(),updateStat);
 statRoutes.delete("/:id", adminAuth, deleteStat);
 
 export  {statRoutes}
